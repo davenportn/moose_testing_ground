cc2cb48a1a0045d95683d6d2b69de59886d1faf4
Marc Kuniansky <mlkuniansky@gmail.com>
2016-10-10 15:15:16 -0400

    Removed some folder nesting that was causing problems on windows because of stupid, arbitrary limits. Also edited some text.

--- android_proj_idunno/app/src/main/java/com/mobile/idunno/idunnosometestapp/MainActivity.java
+++ android_proj_idunno/app/src/main/java/com/mobile/idunno/idunnosometestapp/MainActivity.java
@@ -1,15 +1,17 @@
 package com.mobile.idunno.idunnosometestapp;
 
 // Sup marc
+//I finally got the supid thing to work, Android Studio is dumb sometimes. But, good news is
+//I had to change some of the file structure, which will def. show up in git.
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
